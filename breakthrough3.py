d86f87b2adc8a95df93802ca7435c5757599c337
Leia <daodao0988@gamil.com>
2022-07-03 19:39:02 +0800

    Second version of strategy available for data download from crawler

--- breakthrough3.py
+++ breakthrough3.py
@@ -1,3 +1,12 @@
+# 價格突破區間順勢策略
+# 交易策略為偵測當日行情並順勢而為
+# 進場:
+#     當價格突破某區間(定義上下界為九點之前的最高最低點)，順勢做多或做空
+#
+# 出場:
+#     停利點30點
+#     停損點10點
+
 import sys
 
 
@@ -9,8 +18,9 @@ class StrategyBase:
         if self.NAME is None:
             raise TypeError('NAME argument must be defined')
 
-    def _getdata(self, filename='D:/Course/python/交易策略/breahthrough/TXFB9I020.csv'):
+    def _getdata(self, filename='D:/Course/python/strategies/crawler/tidied/taiwanfuture_all_TX.csv'):
         """讀取資料"""
+        # 0日期 1商品 2到期 3時間 4價格
         data = open(filename).readlines()
         tidy_data = [line.strip('\n').split(',') for line in data]
         return tidy_data
@@ -24,10 +34,12 @@ class StrategyBase:
             day_data = [i for i in tidy_data if i[0] == date_]
 
             # 取得當日九點前股價資料 判斷行情
-            data_before9 = [int(i[3]) for i in day_data if int(i[1]) < 90000000000]
+            data_before9 = [int(i[4]) for i in day_data if int(
+                i[3]) >= 84500 and int(i[3]) < 90000]
 
             # 取得當日九點到十一點資料
-            data_before11 = [i for i in day_data if int(i[1]) >= 90000000000 and int(i[1]) <= 110000000000]
+            data_before11 = [i for i in day_data if int(
+                i[3]) >= 90000 and int(i[3]) <= 110000]
 
             # 定義上下界為九點之前的最高最低點
             ceil = max(i for i in data_before9)
@@ -42,8 +54,8 @@ class StrategyBase:
             # 開始判斷
             for i in range(len(data_before11)):
                 # 當前時間
-                time = data_before11[i][1]
-                price = int(data_before11[i][3])
+                time = data_before11[i][3]
+                price = int(data_before11[i][4])
                 # 建倉判斷
                 if index == 0:
                     # 多單進場
@@ -106,10 +118,10 @@ class StrategyBase:
         # 總交易次數
         total_num = len(profit_list)
         # 平均損益
-        average_profit = round(total_profit/total_num,2)
+        average_profit = round(total_profit/total_num, 2)
         # 總勝率
         win_num = len([i for i in profit_list if i > 0])
-        win_rate = round(win_num/total_num,2)
+        win_rate = round(win_num/total_num, 2)
 
         # 最大連續虧損
         max_continue_loss = 0
@@ -163,6 +175,7 @@ class StockFuture(StrategyBase):
 
 class StrategyManager:
     """標的類別管理者"""
+
     def __init__(self):
         self._implemented_targets = {}   # 已經實作的標的
         for class_ in StrategyBase.__subclasses__():    # StrategyBase的子類別們
